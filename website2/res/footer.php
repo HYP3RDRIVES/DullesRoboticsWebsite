4380e21e9864fd21c199df66a2b7c50ce6dcbc2a
HYPERDRIVE-Motivator <zain-tech@outlook.com>
2019-06-04 16:01:11 -0500

    Made stuff sound better

--- website2/res/footer.php
+++ website2/res/footer.php
@@ -9,10 +9,10 @@
 	<footer class="page-footer font-small bg-dark">
 		<!-- Name and Year (year automatically updates itself) -->
 	  	<div class="footer-copyright text-center py-3 text-light">
-	    	Dulles Computer Science <?php echo date("Y"); ?>-<?php echo (date("Y")+1) ?><br/>
+	    	Dulles Robotics <?php echo date("Y"); ?>-<?php echo (date("Y")+1) ?><br/>
 	  	</div>
 	  	<div class="footer-copyright text-muted text-center small">
-	  		This is a student social media site. Opinions expressed on this site are not necessarily those held by Fort Bend ISD and shall not be attributed to Fort Bend ISD.
+	  		This is a student built, operated, and hosted site. Opinions expressed on this site are not necessarily those held by Fort Bend ISD and shall not be attributed to Fort Bend ISD.
 	  	</div>
 	</footer>
 	<!-- Footer -->
