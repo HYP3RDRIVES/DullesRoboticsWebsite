fa8c1595dd11028cd8d91b9f2a309c7ffde745a3
HYPERDRIVE-Motivator <zain-tech@outlook.com>
2019-08-18 14:43:56 -0500

    started working on sponsorship stuff

--- website2/res/footer.php
+++ website2/res/footer.php
@@ -12,8 +12,15 @@
 	  	<div class="footer-copyright text-center py-3 text-light">
 	    	Dulles Robotics <?php echo date("Y"); ?>-<?php echo (date("Y")+1) ?><br/>
 	  	</div>
+			<div class="row">
 	  	<div class="footer-copyright text-muted text-center small">
 	  		This is a student built, operated, and hosted site. Opinions expressed on this site are not necessarily those held by Fort Bend ISD and shall not be attributed to Fort Bend ISD.
+			</div>
+				 <div class="row">
+					 Special Thanks to our sponsors:
+					 <div class="row">
+						 <a href="wm.com"data-toggle="tooltip" data-placement="top">Waste Management<img src="wm.png"></a>
+				 </div>
 	  	</div>
 	</footer>
 	<!-- Footer -->
