389378628c0cbac94768b3445a4436432a877275
Jwdegames <jacob@enerio.net>
2019-08-20 16:40:33 -0500

    Made footer invisible until at bottom

--- website2/res/footer.php
+++ website2/res/footer.php
@@ -4,10 +4,13 @@
 	Using Bootstrap
 	Modified by Erica Miller, Zain Ahmed, and Jacob Enerio for the Dulles Robotics Club 2019-2020.
 -->
-
-
+	<!-- Empty space for our footer -->
+	<br>
+	<br>
+	<br>
+	<br>
 	<!-- Footer -->
-	<footer class="page-footer font-small bg-dark">
+	<footer class="page-footer font-small bg-dark fixed-bottom" id = "main-footer">
 		<!-- Name and Year (year automatically updates itself) -->
 	  	<div class="footer-copyright text-center py-3 text-light">
 	    	Dulles Robotics <?php echo date("Y"); ?>-<?php echo (date("Y")+1) ?><br/>
@@ -26,3 +29,22 @@
 	</div>
 	</footer>
 	<!-- Footer -->
+	
+	<!-- Footer Script -->
+  <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+	<script>
+	$( document ).ready(function() {
+		$("#main-footer").hide();
+		});
+    $(window).scroll(function() {
+	if($(window).scrollTop() + $(window).height() > $(document).height() - 5) {
+       $("#main-footer").show();
+	   //$("#main-footer").show(1000);
+		}
+	else {
+		$("#main-footer").hide();
+		//$("#main-footer").hide(1000);
+	}
+	});
+	</script>
+	<!-- Footer Script -->
