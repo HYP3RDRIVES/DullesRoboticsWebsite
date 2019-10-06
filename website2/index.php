8c833da7f579a623037bea6e414dbff61b352bb9
Xcallibur232 <xcalliburbusiness@gmail.com>
2019-10-05 22:52:20 -0500

    The toast will now appear above other elements on the index page.
    - z-index is now used to order elements
    - realigned elements on the toast

--- website2/index.php
+++ website2/index.php
@@ -64,16 +64,16 @@
      <div class="toast__content">
   <!--     <p class="toast__type">Due Payment is now Open!</p> -->
        <p class="toast__type">Due payment closes soon!</p>
-       <p class="toast__message">Don't forget to pay dues for the 2019-2020 school year! the button below to pay via Revtrak.</p>
+       <p class="toast__message">Don't forget to pay dues for the 2019-2020 school year! Press the button below to pay via Revtrak.</p>
       <a class="btn btn-danger" href="https://fortbendisd.revtrak.net/FBISD-Schools-1082/High-School/DHS/dhs-robotics/#/list" target="_blank" onclick="toggle_visibility('notibox');">Pay Dues</a>
     <!--   <a class="btn btn-primary" href="https://fortbendisd.revtrak.net/FBISD-Schools-1082/High-School/DHS/dhs-robotics/#/list" target="_blank" onclick="toggle_visibility('notibox');">Pay Dues</a> -->
      </div>
      <div class="toast__close">
-       <a onclick="toggle_visibility('notibox');" >
-       <svg version="1.1" xmlns="https://www.w3.org/2000/svg" viewBox="0 0 15.642 15.642" xmlns:xlink="https://www.w3.org/1999/xlink" enable-background="new 0 0 15.642 15.642">
-     <path fill-rule="evenodd" d="M8.882,7.821l6.541-6.541c0.293-0.293,0.293-0.768,0-1.061  c-0.293-0.293-0.768-0.293-1.061,0L7.821,6.76L1.28,0.22c-0.293-0.293-0.768-0.293-1.061,0c-0.293,0.293-0.293,0.768,0,1.061  l6.541,6.541L0.22,14.362c-0.293,0.293-0.293,0.768,0,1.061c0.147,0.146,0.338,0.22,0.53,0.22s0.384-0.073,0.53-0.22l6.541-6.541  l6.541,6.541c0.147,0.146,0.338,0.22,0.53,0.22c0.192,0,0.384-0.073,0.53-0.22c0.293-0.293,0.293-0.768,0-1.061L8.882,7.821z"></path>
-   </svg>
- </a>
+       <a onclick="toggle_visibility('notibox');">
+       <svg +version="1.1" xmlns="https://www.w3.org/2000/svg" viewBox="0 0 15.642 15.642" xmlns:xlink="https://www.w3.org/1999/xlink" enable-background="new 0 0 15.642 15.642">
+         <path fill-rule="evenodd" d="M8.882,7.821l6.541-6.541c0.293-0.293,0.293-0.768,0-1.061  c-0.293-0.293-0.768-0.293-1.061,0L7.821,6.76L1.28,0.22c-0.293-0.293-0.768-0.293-1.061,0c-0.293,0.293-0.293,0.768,0,1.061  l6.541,6.541L0.22,14.362c-0.293,0.293-0.293,0.768,0,1.061c0.147,0.146,0.338,0.22,0.53,0.22s0.384-0.073,0.53-0.22l6.541-6.541  l6.541,6.541c0.147,0.146,0.338,0.22,0.53,0.22c0.192,0,0.384-0.073,0.53-0.22c0.293-0.293,0.293-0.768,0-1.061L8.882,7.821z"></path>
+       </svg>
+     </a>
      </div>
    </div>
 
