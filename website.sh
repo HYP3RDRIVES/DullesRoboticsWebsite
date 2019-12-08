a21b356ec4e8ee82305fe3a24d274a8dc1de08b8
loonatic <l.oony@aol.com>
2019-12-08 15:50:24 -0600

    Added dev config files. If you're locally testing, note it's for port 8069 instead!

--- website.sh
+++ website.sh
@@ -1,2 +1,5 @@
-git pull origin master
-cd ./site/ && php -S 0.0.0.0:8000
+## NOTE: Remember this is for the development version of the website!
+## https://dev.dullesrobotics.com
+
+git pull
+cd ./site/ && php -S 0.0.0.0:8069
