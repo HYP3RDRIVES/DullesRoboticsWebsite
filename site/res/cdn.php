b58804499c1f5701e10b86d49c702c2388c02f6c
HYPERDRIVE-Motivator <zain-tech@outlook.com>
2020-03-25 00:27:18 -0500

    some fixes

--- site/res/cdn.php
+++ site/res/cdn.php
@@ -1,7 +1,7 @@
 <!-- Bootstrap CDN Resources -->
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://aspencdn.me/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script src="https://aspencdn.me/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
 
 <!--
--- site/res/homeImageCarousel.php
+++ site/res/homeImageCarousel.php
@@ -29,7 +29,7 @@
     <!-- <ol class="carousel-indicators" style="bottom:70px;"><i class="fas fa-arrow-circle-down" style="font-size:40px;"></i></ol> -->
     <div class="carousel-item active" style=" height: 100vh;">
       <!--  <img class="d-block w-100 indxcrop" src="https://drc.aspencdn.me/photos/txchadrive.jpg" alt="Third slide" > -->
-      <div class="fill carousel-image" style="background-image: url('https://drc.aspencdn.me/photos/txchadrive.jpg'); background-attachment: fixed;"></div>
+      <div class="fill carousel-image" style="background-image: url('/img/txchadrive.jpg'); background-attachment: fixed;"></div>
 
       <div class="imgtxt-container">
         <div class="carousel-caption">
@@ -43,7 +43,7 @@
       </div>
     </div>
     <div class="carousel-item" style="height: 100vh;">
-        <div class="fill carousel-image" style="background-image: url('https://drc.aspencdn.me/photos/stateRAS.jpg'); background-attachment: fixed;"></div>
+        <div class="fill carousel-image" style="background-image: url('/img/stateRAS.jpg'); background-attachment: fixed;"></div>
       <div class="imgtxt-container">
         <div class="carousel-caption">
           <h2 class="display-2 c-header animated fadeInDown" style="font-size:100px;">Dulles Robotics </h2>
@@ -54,7 +54,7 @@
 
     <div class="carousel-item" style="height: 100vh;">
       <!-- <img class="d-block w-100 " src="https://drc.aspencdn.me/photos/txpasdrive.jpg" alt="Third slide" > -->
-        <div class="fill carousel-image" style="background-image: url('https://drc.aspencdn.me/photos/txpasdrive.jpg'); background-attachment: fixed;"></div>
+        <div class="fill carousel-image" style="background-image: url('/img/txpasdrive.jpg'); background-attachment: fixed;"></div>
 
       <div class="imgtxt-container">
         <div class="carousel-caption">
